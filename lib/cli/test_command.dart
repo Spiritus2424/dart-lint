9b953eb6da84d5c79c07f93e595c00dadbe81c22
Ahmad Faour <98.ahmad.faour@gmail.com>
2024-08-11 20:35:32 -0400

    starting my own unopiniated dart lint/format project

---
+++ lib/cli/test_command.dart
@@ -0,0 +1,11 @@
+import 'package:args/command_runner.dart';
+
+class TestCommand extends Command {
+  @override
+  // TODO: implement description
+  String get description => "Here for test";
+
+  @override
+  // TODO: implement name
+  String get name => "Test Command";
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:dart_lint/cli/test_command.dart';
+
+void main(List<String> args) {
+  TestCommand testCommand = TestCommand();
+
+  print("Hellow World");
+}
